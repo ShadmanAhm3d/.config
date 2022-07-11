9e351a648f462608d68e5c3f41eb4fe66f060261
ShadmanAhm3d <shadman01010@gmail.com>
2022-07-11 16:57:36 +0530

    Updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main --config=~/.config/polybar/polybar-rounded/config.ini &
-polybar second --config=~/.config/polybar/polybar-rounded/config.ini &
+polybar main --config=~/.config/polybar/polybar-square/config.ini &
+polybar second --config=~/.config/polybar/polybar-square/config.ini &
 
 echo "Polybar launched..."
